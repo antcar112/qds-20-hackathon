2a751fbc48935f3a6ff4f4f14078071d59257526
Anthony Caron <antcar1992@gmail.com>
2020-01-11 21:52:11 -0800

    added router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import theme from './styles/Theme';
 import GlobalStyles from './styles/GlobalStyles';
+import FileUpload from './pages/FileUpload';
+import Graph from './pages/Graph';
+import Map from './pages/Map';
 
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
 			<GlobalStyles />
-			<div className="App">
-				<p>Content goes here.</p>
-			</div>
+			<Router>
+				<div className="App">
+					<p>Content goes here.</p>
+					<Route exact path="/">
+						<FileUpload />
+					</Route>
+					<Route path="/graph">
+						<Graph />
+					</Route>
+					<Route path="/map">
+						<Map />
+					</Route>
+				</div>
+			</Router>
 		</ThemeProvider>
 	);
 }
---
+++ src/pages/FileUpload.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+
+const FileUpload = () => {
+	return <Card>File Upload</Card>;
+};
+
+export default FileUpload;
