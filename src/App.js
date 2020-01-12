5e8ff9fd3a8097ebdd1eb67409892d52556e5eb5
Jason Nguyen <jaseonwing@gmail.com>
2020-01-11 21:27:30 -0800

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
+//import NavDropdown from 'react-bootstrap/NavDropdown';
 import { ThemeProvider } from 'styled-components';
 
+import NavComponent from './NavComponent';
 import theme from './styles/Theme';
 import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
-	return (
-		<ThemeProvider theme={theme}>
-			<GlobalStyles />
-			<div className="App">
-				<p>Content goes here.</p>
-			</div>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+
+      <div className="App">  
+        <NavComponent>
+        </NavComponent>
+        <p>Content goes here.</p>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
