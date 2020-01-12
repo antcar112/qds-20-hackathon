da5583bc9fa8f9a3ed8b5972a5845f012efd57ce
Jason Nguyen <jaseonwing@gmail.com>
2020-01-12 11:24:30 -0800

    add apex2

--- src/pages/Bunching.js
+++ src/pages/Bunching.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Card from '../components/Card';
-import Apex from '../components/Apex';
+import Apex2 from '../components/Apex2';
 import { data } from '../data/data';
+import { data2 } from '../data/data2';
 
 const Bunching = () => {
 	return (
 		<Card>
-			<Apex data={data} />
+			<Apex2 data={data} data2={data2} />
 		</Card>
 	);
 };
