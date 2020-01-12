9e33a3e53a52e3410154e504a152e5b202b4c44f
Anthony Caron <antcar1992@gmail.com>
2020-01-11 21:06:33 -0800

    added theming

---
+++ src/styles/Theme.js
@@ -0,0 +1,15 @@
+const theme = {
+	color : {
+		text          : '#000000',
+		textSecondary : '#777777',
+		bg            : '#dddddd',
+		bgCard        : '#ffffff',
+		primary       : '#ff0000'
+	},
+	font  : {
+		heading : '"Roboto"',
+		body    : '"Roboto"'
+	}
+};
+
+export default theme;
